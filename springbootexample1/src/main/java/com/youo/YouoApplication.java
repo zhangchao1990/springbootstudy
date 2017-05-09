8380abc7cd05877cda380dc39edfc3e5b2b3b664
张超 <zhangchao@youotech.com>
2017-05-09 20:26:24 +0800

    第一的案例

---
+++ springbootexample1/src/main/java/com/youo/YouoApplication.java
@@ -0,0 +1,12 @@
+package com.youo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouoApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(YouoApplication.class, args);
+	}
+}

b08149fc408d7a5622cc2842a22f0e292f628e10
张超 <zhangchao@youotech.com>
2017-05-13 15:29:09 +0800

    spring error set

---
+++ springbootexample7/src/main/java/com/youo/exception/MyException.java
@@ -0,0 +1,18 @@
+package com.youo.exception;
+
+/**
+ * @author 
+ * 
+ */
+public class MyException extends Exception {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MyException(String message) {
+        super(message);
+    }
+
+}

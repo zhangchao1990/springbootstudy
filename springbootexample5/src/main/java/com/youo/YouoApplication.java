cf3093880897f0b8e2e9922abbd6232ddae1142c
张超 <zhangchao@youotech.com>
2017-05-13 14:58:15 +0800

    spring boot freemarker

---
+++ springbootexample5/src/main/java/com/youo/YouoApplication.java
@@ -0,0 +1,13 @@
+package com.youo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouoApplication {
+
+	
+	public static void main(String[] args){
+		SpringApplication.run(YouoApplication.class, args);
+	}
+}
---
+++ springbootexample5/src/test/java/com/youo/YouoApplicationTests.java
@@ -0,0 +1,106 @@
+package com.youo;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.youo.web.HelloController;
+
+
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration
+public class YouoApplicationTests {
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(
+				new HelloController()).build();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
+	}
+
+	@Test
+	public void testUserController() throws Exception {
+//  	测试UserController
+		RequestBuilder request = null;
+
+		// 1、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")));
+
+		// 2、post提交一个user
+		request = post("/users/")
+				.param("id", "1")
+				.param("name", "测试大师")
+				.param("age", "20");
+		mvc.perform(request)
+//				.andDo(MockMvcResultHandlers.print())
+				.andExpect(content().string(equalTo("success")));
+
+		// 3、get获取user列表，应该有刚才插入的数据
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+
+		// 4、put修改id为1的user
+		request = put("/users/1")
+				.param("name", "测试终极大师")
+				.param("age", "30");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")));
+
+		// 5、get一个id为1的user
+		request = get("/users/1");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+
+		// 6、del删除id为1的user
+		request = delete("/users/1");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")));
+
+		// 7、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")));
+
+	}
+
+}
